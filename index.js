30bb8befcadfe83382a14d29327549e0350a4a5a
norton-kb <biinorton080@gmail.com>
2024-11-05 11:27:04 +0300

    practising using find

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    const win = record.find(function(game){
+        return game.result === "W";
+    })
+    return win ? win.year : undefined;
+}
+console.log(superbowlWin(record));
+
